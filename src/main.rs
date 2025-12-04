8d41537ae7cb362537b391c49bc59f0fb64a2434
zhuchok <zhuchok@duck.com>
2025-12-04 19:41:08 +0300

    first steps in chifer, ready skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
-use std::error::Error;
+use std::path::{Path, PathBuf};
 
-use clap::Parser;
+use clap::{ArgGroup, Parser, command};
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
+#[command(author = "zhuchops", version = "0.1", about = "simple cli to en\\decrypt files.", long_about = None)]
+#[command(group(
+    ArgGroup::new("mode")
+    .required(true)
+    .args(["encrypt", "decrypt"])
+))]
 struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
+    #[arg(short, long)]
+    encrypt: bool,
+
+    #[arg(short, long)]
+    decrypt: bool,
+
+    #[arg(name = "PATH_TO_FILE")]
+    path: PathBuf,
 }
+fn main() -> Result<(), anyhow::Error> {
+    let args = Cli::parse();
 
-#[derive(Debug)]
-struct CustomError(String);
+    let password = rpassword::prompt_password("Enter password: ")?;
 
-fn main() -> Result<(), CustomError> {
-    let cli = Cli::parse();
-
-    let content = std::fs::read_to_string(&cli.path)
-        .map_err(|err| CustomError(format!("Error reading \'{:?}\': {}", cli.path, err)))?;
-    println!("File content:\n{}", content);
-
-    println!("Matches:");
-    for line in content.lines() {
-        if line.contains(&cli.pattern) {
-            println!("{}", line)
-        }
+    if args.encrypt {
+        encrypt_file(&args.path, password.as_str())?;
+    } else {
+        decrypt_file(&args.path, password.as_str())?;
     }
     Ok(())
+}
+
+fn derive_key(user_password: String) {
+    todo!()
+}
+
+fn encrypt_file(path: &PathBuf, password: &str) -> Result<(), anyhow::Error> {
+    todo!()
+}
+fn decrypt_file(path: &Path, password: &str) -> Result<(), anyhow::Error> {
+    todo!()
 }
